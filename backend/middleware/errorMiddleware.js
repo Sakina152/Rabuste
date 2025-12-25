e86b7e329f9557dcb6f407ef00fe554e2c61a6b9
Sakina Dhilawala <skdhilawala1280@gmail.com>
2025-12-25 20:19:35 +0530

    updated errorMiddleware

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -6,11 +6,38 @@ const notFound = (req, res, next) => {
 };
 
 const errorHandler = (err, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  res.status(statusCode).json({
-    message: err.message,
+  let error = { ...err };
+  error.message = err.message;
+
+  // Log to console for dev
+  console.error(err);
+
+  // Mongoose bad ObjectId
+  if (err.name === 'CastError') {
+    const message = `Resource not found`;
+    error = new Error(message);
+    error.statusCode = 404;
+  }
+
+  // Mongoose duplicate key
+  if (err.code === 11000) {
+    const message = 'Duplicate field value entered';
+    error = new Error(message);
+    error.statusCode = 400;
+  }
+
+  // Mongoose validation error
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map(val => val.message);
+    error = new Error(message);
+    error.statusCode = 400;
+  }
+
+  res.status(error.statusCode || 500).json({
+    success: false,
+    error: error.message || 'Server Error',
     stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack,
   });
 };
 
-module.exports = { notFound, errorHandler };
+export { notFound, errorHandler };
