16ca7b703c121baa01b276f126dd31a6aff18e8c
HHK5253 <hatimkutarwadliwala@gmail.com>
2026-01-07 20:19:23 +0530

    cloudinary set up

---
+++ backend/config/multer.js
@@ -0,0 +1,18 @@
+const multer = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("./cloudinary");
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "website-assets",
+    allowed_formats: ["jpg", "png", "jpeg", "webp"],
+    transformation: [
+      { quality: "auto", fetch_format: "auto" },
+    ],
+  },
+});
+
+const upload = multer({ storage });
+
+module.exports = upload;
